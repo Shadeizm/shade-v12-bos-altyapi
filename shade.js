86b8ddafd434faf57f050ba232b8b2ae2f1c504b
Shâdeizm <101464304+Shadeizm@users.noreply.github.com>
2022-03-13 09:37:23 +0300

    Update shade.js

--- shade.js
+++ shade.js
@@ -1,7 +1,16 @@
 const Discord = require("discord.js")
 const client = new Discord.client
+var prefix = ayarlar.prefix
 
 client.on("ready", () => {
   client.user.setActivity(`Coded By Shâde`);
   console.log(`${props.help.name} Yüklendi`);
 });
+
+client.on('message', message => {
+  if (message.content.startsWith(prefix + "yanıtverme")) {
+    message.channel.send('cevap');
+  }
+});
+
+client.login("token")
